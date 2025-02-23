52374ff01a768396390645d6a8684a13c876ffe2
InvisiBug <matthewjkavanagh@gmail.com>
2025-02-23 20:33:15 +0000

    shadcn  loaded

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-// import { defineConfig } from "vite";
-// import ssr from "vike/plugin";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), ssr()],
-//   ssr: {
-//     noExternal: ["styled-components", "@emotion/*"],
-//   },
-// });
-
 import { defineConfig } from "vite";
-import commonjs from "vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
